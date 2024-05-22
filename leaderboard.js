a6883fc356c5fcca7b698b654afd0094ff641efd
Hannah Thompson <ht1226@york.ac.uk>
2024-05-22 11:48:06 +0100

    tried to add new server

--- leaderboard.js
+++ leaderboard.js
@@ -1,25 +1,26 @@
-fetch('http://34.39.12.110:3000/leaderboard')
+fetch('https://score-board-afe96bddb988.herokuapp.com/get_scores')
   .then(response => response.json())
   .then(data => {
+    console.log(data);
     renderLeaderboard(data);
   })
   .catch(error => {
     console.error('Error fetching leaderboard:', error);
   });
 
-  function renderLeaderboard(data) {
-    const leaderboardContainer = document.getElementById('leaderboard-container');
-    leaderboardContainer.innerHTML = '';
-  
-    const leaderboardList = document.createElement('ul');
-    leaderboardList.classList.add('leaderboard-list');
-  
-    data.forEach(entry => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${entry.username}: ${entry.score}`;
-      leaderboardList.appendChild(listItem);
-    });
-  
-    leaderboardContainer.appendChild(leaderboardList);
-  }
-  
+function renderLeaderboard(data) {
+  const leaderboardContainer = document.getElementById('leaderboard-container');
+  leaderboardContainer.innerHTML = '';
+
+  const leaderboardList = document.createElement('ul');
+  leaderboardList.classList.add('leaderboard-list');
+
+  data.forEach(entry => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${entry.username}: ${entry.score}`;
+    leaderboardList.appendChild(listItem);
+  });
+
+  leaderboardContainer.appendChild(leaderboardList);
+}
+
